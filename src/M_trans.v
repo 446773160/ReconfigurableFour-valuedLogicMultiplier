a20b1bcc6a09de23943fcd0f02d84e1b7c31588c
liaosc <you@example.com>
2025-03-11 13:27:02 +0800

    init

--- src/M_trans.v
+++ src/M_trans.v
@@ -1,11 +1,11 @@
-module M_trans_cell (
+module M_trans_module (
     input        clk,
     input        rst_n,
     input  [1:0] a_i,
     input  [1:0] b_i,
     output reg [1:0] m_i
 );
-// reg [1:0] m_i;
+
 always @(posedge clk or negedge rst_n) begin
     if (~rst_n) begin
         m_i <= 2'b0;
@@ -13,12 +13,12 @@ always @(posedge clk or negedge rst_n) begin
     else begin
         if (a_i == 2'b00 && b_i == 2'b00)
             m_i <= 2'b10;
+        else if (a_i == 2'b00 && b_i == 2'b10)
+            m_i <= 2'b00;
         else if (a_i == 2'b10 && b_i == 2'b00)
             m_i <= 2'b00;
         else if (a_i == 2'b10 && b_i == 2'b10)
             m_i <= 2'b10;
-        else if (a_i == 2'b00 && b_i == 2'b10)
-            m_i <= 2'b00;
         else
             m_i <= 2'b01;
     end
@@ -38,8 +38,8 @@ module M_trans #(
 
 genvar i;
 generate
-    for (i = 0; i < p; i = i + 1) begin : M_cell
-        M_trans_cell u_M_trans_cell(clk, rst_n, a_i[2*i+1:2*i], b_i, m_i[2*i+1:2*i]); 
+    for (i = 0; i < p; i = i + 1) begin : M_module
+        M_trans_module u_M_trans_module(clk, rst_n, a_i[2*i+1:2*i], b_i, m_i[2*i+1:2*i]); 
     end
 endgenerate
 
